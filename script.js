ca513901d8d841080da8e805d34aba62fa57b221
Nunzio Baldares <bvldares@outlook.com>
2022-09-24 14:51:08 +0200

    quasi finito

--- script.js
+++ script.js
@@ -2,9 +2,25 @@ const nav = document.querySelector("nav")
 
 
 document.getElementById("menu-toggle").addEventListener("click", function(){
-    if(nav.style.display === "none"){
-        nav.style.display = "block"
+    if(nav.style.display === "block"){
+        nav.style.display = "none"
     }else{
-        nav.style.display ="none"
+        nav.style.display ="block"
     }
 })
+
+
+
+var screen = window.matchMedia("(min-width:765px)")
+
+function navSwitch(screen) {
+    if(screen){
+        nav.style.display = "block"
+        
+        document.querySelector(".hamburger").style.display ="none"
+    }
+}
+
+
+navSwitch(screen)
+screen.addListener(navSwitch)
